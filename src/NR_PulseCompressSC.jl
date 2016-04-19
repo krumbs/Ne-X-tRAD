855031fb3f3b9509150735047ee7769e49d60ba9
Stephanie Jonkers <stephaniecjonkers@outlook.com>
2016-04-19 16:17:33 +0200

    Update NR_PulseCompressSC.jl
    Added the option of returning the analytic signal by means of an extra boolean parameter to the function.

--- src/NR_PulseCompressSC.jl
+++ src/NR_PulseCompressSC.jl
@@ -7,83 +7,65 @@ include("NR_ReadRefSig.jl")
 export readRawData
 export readRefSig
 
-function SCPC(nPulses_::Int, rawDataFile_::AbstractString, refSigFile_::AbstractString)
+function SCPC(nPulses_::Int, rawDataFile_::AbstractString, refSigFile_::AbstractString, analytic_::Bool=false)
 
 ## Read in refernce signal from file
-dataRaw = readRawData(nPulses_, rawDataFile_)	#"/home/stephaniejonkers/NetRAD/e10_10_21_1927_55_P1_1_130000_S0_1_2047_node3.bin"
+d = readRawData(nPulses_, rawDataFile_)	# Function that processes the radar data
 
 ## Read the chirp from the txt file
-refSig = readRefSig(refSigFile_);		#"/home/stephaniejonkers/NetRAD/refSigN3N3Pl5000.txt"
+r = readRefSig(refSigFile_);		# Function that processes the reference signal 
 	
-# Array and Variable Initializations
-#dataPostiveHalf		= zeros(Complex64, nPulses_, 1024);
-dataMatched 		= zeros(Complex64, nPulses_, 2048);
-dataIFFT		= zeros(Complex64, nPulses_, 2048);
-#x 			= zeros(Complex64, nPulses_, 2048);
-# Pulse Compression
-P = plan_fft(dataRaw[1,:]);
+## Array and Variable Initializations
+dPostiveHalf = zeros(Complex64, nPulses_, 1024);
+dMatched = zeros(Complex64, nPulses_, 2048);
+dIFFT = zeros(Complex64, nPulses_, 2048);
+dOut = zeros(Complex64, nPulses_, 2048);
+
+## Pulse Compression
+P = plan_fft(d[1,:]);
 for n = 1:nPulses_
-	dataMatched[n,:] = refSig.*(P*dataRaw[n,:]);
+	dMatched[n,:] = r.*(P*d[n,:]);
 end
 
-# Clear memory
-dataRaw		= 0;
-refSigRaw 	= 0;
-refSig 		= 0;
+## Clear memory
+d = 0;
+r = 0;
 gc()
 
-P = plan_ifft(dataMatched[1,:]);
-for n = 1:nPulses_
-	dataIFFT[n,:] = P*dataMatched[n,:];
+P = plan_ifft(dMatched[1,:]);
+
+## Branch for analytic signal
+if analytic_==false
+	for n = 1:nPulses_
+		dIFFT[n,:] = P*dMatched[n,:];
+	end
+
+	## Clear memory
+	dMatched = 0;
+	gc()
+	return dIFFT
+else
+	## Create the analytic signal transform
+	dMatched[:,2:2048] = 2.*dMatched[:,2:2048];
+
+	## Flatten the negative frequencies
+	dPostiveHalf = hcat(dPostiveHalf,dMatched[:,1025:end]);
+
+	## Transform back into time domain
+	for n = 1:nPulses_
+		dOut[n,:] = ifft(dPostiveHalf[n,:]);
+	end
+
+	## Clear memory
+	dMatched = 0;
+	dPositiveHalf = 0;
+	gc()
+	return dOut
 end
 
-# Clear memory
-dataMatched	= 0;
-gc()
-
-#=
-# Create the analytic signal transform
-dataMatched[:,2:2048] 	= 2.*dataMatched[:,2:2048];
-
-# Flatten the negative frequencies
-dataPostiveHalf 	= hcat(dataPostiveHalf,dataMatched[:,1025:end]);
-
-# Transform back into time domain
-for n = 1:nPulses_
-	x[n,:] 		= ifft(dataPostiveHalf[n,:]);
-end
-=#
-
-# Clear memory
-dataMatched		= 0;
-#dataPostiveHalf	= 0;
-gc()
-
-return dataIFFT
 end
 tic()
-data = SCPC(8000, "/home/stephaniejonkers/NetRAD/e10_10_21_1927_55_P1_1_130000_S0_1_2047_node3.bin", "/home/stephaniejonkers/NetRAD/refSigN3N3Pl5000.txt")
+data = SCPC(10000, "/home/jnkste003/thesis_code/Ne-X-tRAD/data/e10_10_21_1927_55_P1_1_130000_S0_1_2047_node3.bin", "/home/jnkste003/thesis_code/Ne-X-tRAD/data/refSigN3N3Pl5000.txt", true)
 toc()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
