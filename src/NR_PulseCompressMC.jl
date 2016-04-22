d097a97c1e499dd7a6498b8c676704dea0d25e02
Stephanie Jonkers <stephaniecjonkers@outlook.com>
2016-04-22 17:08:22 +0200

    Update NR_PulseCompressMC.jl
    Package DistributeArrays v0.2.0 does not include a function describing chunks and thus NR_Distribute() needed to remove using chunks

--- src/NR_PulseCompressMC.jl
+++ src/NR_PulseCompressMC.jl
@@ -5,49 +5,45 @@
 Pkg.add("DistributedArrays")
 using DistributedArrays
 
-include("/home/stephanie/Desktop/NR_ReadBinFile.jl") 
-include("/home/stephanie/Desktop/NR_ReadRefSig.jl")
+include("NR_ReadBinFile.jl") 
+include("NR_ReadRefSig.jl")
 export readRawData
 export readRefSig
 
 #---------------------------------------------------------------------------------------------------------------------------------
 ## Altered distribute "DistributedArrays.jl" distribute() function to distribute the raw data matrix into specified distribution
 function NR_Distribute(A::AbstractArray, mydist::AbstractArray; myprocs=workers()[1:min(nworkers(), maximum(size(A)))])
-    owner = myid()
-    rr = RemoteRef();
-    put!(rr, A);
-    d = DArray(size(A), myprocs, mydist) do I
-        remotecall_fetch(()->fetch(rr)[I...], owner);
-    end
-    # Ensure that all workers have fetched their localparts.
-    for chunk in d.chunks
-        wait(chunk);
-    end
-    return d
+	owner = myid()
+	rr = RemoteRef();
+	put!(rr, A);
+	d = DArray(size(A), myprocs, mydist) do I
+		remotecall_fetch(()->fetch(rr)[I...], owner);
+	end
+	return d
 end
 #---------------------------------------------------------------------------------------------------------------------------------	
 ## Serial Pulse Compression function to run on multiple cores
-@everywhere function NR_Correlate(nPulses_::Int, refSig_::AbstractArray, rawData_::AbstractArray; analytic_::Bool = false)
-	# Array Initialization
+@everywhere function NR_Correlate(np_::Int, refSig_::AbstractArray, rawData_::AbstractArray; analytic_::Bool = false)
+	## Array Initialization
 	local d;
 	local pl;
-	d = zeros(nPulses_, 2048)*im;
+	d = zeros(np_, 2048)*im;
 	pl = plan_fft(rawData_[1,:]);
 
-	# Loop to correlate frequency domain refSig and data array, and convert back to time domain
-	for n = 1:nPulses_
+	## Loop to correlate frequency domain refSig and data array, and convert back to time domain
+	for n = 1:np_
 		d[n,:] = (refSig_).*(pl*rawData_[n,:]);
 	end
-	# Flattens the negative half of the spectrum to create the analytic signal
+	## Flatten the negative half of the spectrum to create the analytic signal
 	if (analytic_ == 1)
 		local s;
-		s = convert(Int64, nPulses_/2);
+		s = convert(Int64, np_/2);
 		for i = 1:n_pulses
 			d[i,1:s] = zeros(s,1);
 		end
 	end
 	pl = plan_ifft(d[1,:]);
-	for n = 1:nPulses_
+	for n = 1:np_
 		d[n,:] = (pl*d[n,:]);
 	end
 	return d
@@ -56,36 +52,35 @@ end
 #---------------------------------------------------------------------------------------------------------------------------------	
 ## Parallel Pulse Compression function
 
-function MCPC(nPulses_::Int, rawDataFile_::AbstractString, refSigFile_::AbstractString, analytic_::Bool)
-	# Array and Variable Initializations
+function MCPC(np_::Int, rawDataFile_::AbstractString, refSigFile_::AbstractString, analytic_::Bool)
+	## Array and Variable Initializations
 	local PCchunk;
 	local Ddata;
-	local dataRaw;
-	local refSig;
+	local dRaw;
+	local rSig;
 	local refs;
 	local DdataArray;
-	#local DdataOut;
 
 	P = nprocs()-1;  
-	PCchunk = convert(Int, floor(nPulses_/P));
-	Ddata = zeros(nPulses_,2048);
+	PCchunk = convert(Int, floor(np_/P));
+	Ddata = zeros(np_,2048);
 
-	# Read the data from the bin file
-	dataRaw = readRawData(nPulses_,rawDataFile_);
+	## Read the data from the bin file
+	dRaw = readRawData(np_,rawDataFile_);
 	
-	# Read the chirp from the txt file
-	refSig = readRefSig(refSigFile_);
+	## Read the chirp from the txt file
+	rSig = readRefSig(refSigFile_);
 
-	# Distribute the raw data split in the row dimension
-	Ddata = NR_Distribute(dataRaw, [P,1]);
+	## Distribute the raw data split in the row dimension
+	Ddata = NR_Distribute(dRaw, [P,1]);
 
-	# Clear local raw data memory
-	dataRaw = 0;
+	## Clear local raw data memory
+	dRaw = 0;
 	gc()
 
-	refs = [(@spawnat (procs(Ddata))[w] NR_Correlate(PCchunk, refSig, localpart(Ddata)), analytic_) for w=1:P];
+	refs = [(@spawnat (procs(Ddata))[w] NR_Correlate(PCchunk, rSig, localpart(Ddata)), analytic_) for w=1:P];
 
-	# Fetch all the remote references
+	## Fetch all the remote references
 	DdataArray = Array{Any,1};
 	DdataArray = pmap(fetch,refs);
 
@@ -93,7 +88,7 @@ function MCPC(nPulses_::Int, rawDataFile_::AbstractString, refSigFile_::Abstract
 	Ddata = 0;
 	refs = 0;
 	gc()
-
+#=
 	## Create a file for Memory Mapping
 
 	myFile = open("NR_PCdata.bin", "w+");
@@ -107,15 +102,11 @@ function MCPC(nPulses_::Int, rawDataFile_::AbstractString, refSigFile_::Abstract
 	gc()
 	close(myFile);
 
-#=
-	return DdataArray
 =#
+	return DdataArray
+
 end
 ## Run the Pulse Compression function
-data = MCPC(2048*35, "/home/stephanie/Desktop/e10_10_21_1927_55_P1_1_130000_S0_1_2047_node3.bin", "/home/stephanie/Desktop/refSigN3N3Pl5000.txt", true)
-
-
-
-
-
-
+#tic()
+#data = MCPC(130000, "/home/jnkste003/thesis_code/Ne-X-tRAD/data/e10_10_21_1927_55_P1_1_130000_S0_1_2047_node3.bin", "/home/jnkste003/thesis_code/Ne-X-tRAD/data/refSigN3N3Pl5000.txt", true)
+#toc()
